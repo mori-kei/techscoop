d39d0cb97cc505c5974901566356be7f6089b672
mori-kei <morimeru13@gmail.com>
2025-04-30 17:48:00 +0900

    scssの導入を行なった

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="Header">
+    <div class="Header__Logo">
+      <p>画像が入ります</p>
+    </div>
+    <h1 class="Header__Title">TechScoop</h1>
+  </header>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+@use "./style.scss";
+</style>
