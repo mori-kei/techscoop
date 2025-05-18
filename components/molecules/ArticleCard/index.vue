3412b77ac85cbd9b8522c8ef39ba1418133c2cf8
mori-kei <morimeru13@gmail.com>
2025-05-18 11:45:56 +0900

    一旦実装

--- components/molecules/ArticleCard/index.vue
+++ components/molecules/ArticleCard/index.vue
@@ -2,9 +2,59 @@
   <div class="Article">
     <h2 class="Article__Title">{{ item.title }}</h2>
     <div class="Article__Meta">
-      <span>{{ formatDate(item.created_at) }}</span>
-      <span>いいね: {{ item.likes_count }}</span>
-      <span>ストック: {{ item.stocks_count }}</span>
+      <span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+          <line x1="16" y1="2" x2="16" y2="6"></line>
+          <line x1="8" y1="2" x2="8" y2="6"></line>
+          <line x1="3" y1="10" x2="21" y2="10"></line>
+        </svg>
+        {{ formatDate(item.created_at) }}
+      </span>
+      <span class="likes">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path
+            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+          ></path>
+        </svg>
+        {{ item.likes_count }}
+      </span>
+      <span class="stocks">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+        </svg>
+        {{ item.stocks_count }}
+      </span>
     </div>
     <p class="Article__Summary"></p>
     <div class="Article__Tags">
