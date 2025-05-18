bc874f65e90f527a6432e26d725a1b6b50804402
mori-kei <morimeru13@gmail.com>
2025-05-18 11:29:54 +0900

    実装

--- plugins/msw.client.ts
+++ plugins/msw.client.ts
@@ -1,14 +1,16 @@
 import { worker } from "~/mocks/browser";
+import { handlers } from "~/mocks/handlers";
 
 export default defineNuxtPlugin(() => {
   // 開発環境かどうかをチェック
   if (process.env.NODE_ENV === "development") {
     // MSWを起動
     worker.start({
-      onUnhandledRequest: "bypass", // モックされていないリクエストは通常通り処理
+      onUnhandledRequest: "bypass",
     });
     console.log(
       "[MSW] モックサーバーが起動しました。APIリクエストはインターセプトされます。"
     );
+    console.log("Current NODE_ENV:", process.env.NODE_ENV);
   }
 });
